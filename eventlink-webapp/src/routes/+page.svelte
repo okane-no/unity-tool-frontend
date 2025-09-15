b29ee6000a32490a2295ffdf726fe1c836fa2f26
Stian Aspholm <stian@aspholm.com>
2025-09-15 21:50:42 +0200

    Added check if unity is slow

--- eventlink-webapp/src/routes/+page.svelte
+++ eventlink-webapp/src/routes/+page.svelte
@@ -460,11 +460,13 @@ onMount(async () => {
 		if (!tokenUnity) return;
 		try {
       loadingUnityVerify = true;
+      console.log("Started verify")
 			const result = await verifyUnityAccessToken(tokenUnity);
 			unityLoggedIn = true;
 			userInfo = result;
       user.set(userInfo);
 			loggedIn.set(true);
+      console.log("finished verify")
 			console.log('✅ Verified login:', result);
       loadingUnityVerify = false;
 		} catch (err) {
@@ -801,9 +803,9 @@ onMount(async () => {
 					    <svg class="animate-spin h-5 w-5 text-white mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path></svg>
 				      </button>
               
-            {/if}
+              {/if}
               
-            {:else}
+              {:else}
               <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50" on:click={handleUnityLogin}>Login to Unity</button>
             {/if}
             <div class="flex gap-2">
