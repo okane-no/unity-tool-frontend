23301699c979c4e753ccfefa13650d212e5c8bd0
Stian Aspholm <stian@aspholm.com>
2025-09-15 10:27:55 +0200

    Updated a variablex2

--- eventlink-webapp/src/routes/+page.svelte
+++ eventlink-webapp/src/routes/+page.svelte
@@ -591,7 +591,12 @@ onMount(async () => {
   // Optional: clear/clamp if user somehow sets a date outside the month
   $: if (selectedDate && (selectedDate < minDate || selectedDate > maxDate)) {
     selectedDate = minDate;
-    searchedForEvent = false
+    
+  }
+
+   $: if (selectedDate) {
+    searchedForEvent = false;
+    
   }
 
 </script>
